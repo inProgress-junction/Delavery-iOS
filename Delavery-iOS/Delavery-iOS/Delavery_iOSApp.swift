8710e1c4e62587a8dd0293913b1478f05116feb5
E-know <eknow25@kakao.com>
2024-08-10 21:13:39 +0900

    Init Project

---
+++ Delavery-iOS/Delavery-iOS/Delavery_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Delavery_iOSApp.swift
+//  Delavery-iOS
+//
+//  Created by Inho Choi on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Delavery_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
